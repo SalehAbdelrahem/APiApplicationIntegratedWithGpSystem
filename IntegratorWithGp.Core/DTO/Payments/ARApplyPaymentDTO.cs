e71a81b721e43f09a67976e575e61630b9007470
Salh 15237657 <Salh.15237657@compit.au.edu.eg>
2025-06-07 22:39:58 +0000

    Edit for payment query

--- IntegratorWithGp.Core/DTO/Payments/ARApplyPaymentDTO.cs
+++ IntegratorWithGp.Core/DTO/Payments/ARApplyPaymentDTO.cs
@@ -4,23 +4,33 @@ namespace IntegratorWithGp.Core.DTO.Payments
 {
     public class ARApplyPaymentDTO
     {
-        public decimal AmountApplied { get; set; }
-        public DateTime? AppliedToDocumentDate { get; set; }
-        public DateTime? ApplyDocumentDate { get; set; }
-        public string AppliedToDocTypeName { get; set; }
-        public string AppliedToDocNumber { get; set; }
-        public string DocumentNumber { get; set; }
-        public decimal? ExchangeRate { get; set; }
-        public DateTime? DocumentDate { get; set; }
-        public string ShipmentNumber { get; set; }
-        public string Department { get; set; }
-        public string Branch { get; set; }
-        public string ClientOrderNo { get; set; }
-        public string BillOfLading { get; set; }
-        public string CurrencyID { get; set; }
-        public string CustomerID { get; set; }
-        public string CustomerName { get; set; }
-
+        #region old
+        //public decimal AmountApplied { get; set; }
+        //public DateTime? AppliedToDocumentDate { get; set; }
+        //public DateTime? ApplyDocumentDate { get; set; }
+        //public string AppliedToDocTypeName { get; set; }
+        //public string AppliedToDocNumber { get; set; }
+        //public string DocumentNumber { get; set; }
+        //public decimal? ExchangeRate { get; set; }
+        //public DateTime? DocumentDate { get; set; }
+        //public string ShipmentNumber { get; set; }
+        //public string Department { get; set; }
+        //public string Branch { get; set; }
+        //public string ClientOrderNo { get; set; }
+        //public string BillOfLading { get; set; }
+        //public string CurrencyID { get; set; }
+        //public string CustomerID { get; set; }
+        //public string CustomerName { get; set; }
+        #endregion
+        public string CustomerID { get; set; }           
+        public string InvoiceNumber { get; set; }        
+        public decimal? RemrningAmount { get; set; }     
+        public decimal? CURTRXAM { get; set; }           
+        public decimal? ORTRXAMT { get; set; }           
+        public string CURNCYID { get; set; }             
+        public decimal? XCHGRATE { get; set; }            
+        public decimal? CurrencyAmount { get; set; }     
+        public int? RMDTYPAL { get; set; }
 
     }
 }
--- IntegratorWithGp.Services/Services/PaymentServices/PaymentService.cs
+++ IntegratorWithGp.Services/Services/PaymentServices/PaymentService.cs
@@ -20,26 +20,40 @@ namespace IntegratorWithGp.Services.Services.PaymentServices
         public ApiResponse<List<ARApplyPaymentDTO>> GetARApplyPayment(string customerId)
 
         {
+            #region old 
+            //string query = $@"SELECT
+            //                           [Amount_Applied]
+            //                          ,[Applied_to_Document_Date]
+            //                          ,[Apply_Document_Date]
+            //                          ,[Applied_to_Doc_Type_Name]
+            //                          ,[Applied_to_Doc_Number]
+            //                          ,[Document_Number]
+            //                          ,[Exchange Rate]
+            //                          ,[Document_Date]
+            //                          ,[Shipment number]
+            //                          ,[Department]
+            //                          ,[Branch]
+            //                          ,[Client order No]
+            //                          ,[Bill of lading]
+            //                          ,[Currency ID]
+            //                          ,[Customer_ID]
+            //                          ,[Customer_Name]
+            //                           FROM[dbo].[view_AR_Apply_Detail_AR2]
+            //                           where [Customer_ID]='{customerId}'";
+            #endregion
+           
 
-            string query = $@"SELECT
-                                       [Amount_Applied]
-                                      ,[Applied_to_Document_Date]
-                                      ,[Apply_Document_Date]
-                                      ,[Applied_to_Doc_Type_Name]
-                                      ,[Applied_to_Doc_Number]
-                                      ,[Document_Number]
-                                      ,[Exchange Rate]
-                                      ,[Document_Date]
-                                      ,[Shipment number]
-                                      ,[Department]
-                                      ,[Branch]
-                                      ,[Client order No]
-                                      ,[Bill of lading]
-                                      ,[Currency ID]
-                                      ,[Customer_ID]
-                                      ,[Customer_Name]
-                                       FROM[dbo].[view_AR_Apply_Detail_AR2]
-                                       where [Customer_ID]='{customerId}'";
+            string query = $@"SELECT   [Customer ID]
+                                      ,[Invoice Number]
+                                      ,[Remrning_Amount]
+                                      ,[CURTRXAM]
+                                      ,[ORTRXAMT]
+                                      ,[CURNCYID]
+                                      ,[XCHGRATE]
+                                      ,[Currency amount]
+                                      ,[RMDTYPAL]
+                                       FROM [dbo].[AR_Remaining_Amount]
+                                       where [Customer ID]='{customerId}'";
 
             var data =  _econnectServices.OpenConnectionGp(query);
             var result = new List<ARApplyPaymentDTO>();
@@ -57,23 +71,33 @@ namespace IntegratorWithGp.Services.Services.PaymentServices
                 {
                     result.Add(new ARApplyPaymentDTO
                     {
-                        AmountApplied = row.Field<decimal>("Amount_Applied"),
-                        AppliedToDocumentDate = row.Field<DateTime?>("Applied_to_Document_Date"),
-                        ApplyDocumentDate = row.Field<DateTime?>("Apply_Document_Date"),
-                        AppliedToDocTypeName = row.Field<string>("Applied_to_Doc_Type_Name")?.Trim(),
-                        AppliedToDocNumber = row.Field<string>("Applied_to_Doc_Number")?.Trim(),
-                        DocumentNumber = row.Field<string>("Document_Number")?.Trim(),
-                        ExchangeRate = row.Field<decimal?>("Exchange Rate"),
-                        DocumentDate = row.Field<DateTime?>("Document_Date"),
-                        ShipmentNumber = row.Field<string>("Shipment number")?.Trim(),
-                        Department = row.Field<string>("Department")?.Trim(),
-                        Branch = row.Field<string>("Branch")?.Trim(),
-                        ClientOrderNo = row.Field<string>("Client order No")?.Trim(),
-                        BillOfLading = row.Field<string>("Bill of lading")?.Trim(),
-                        CurrencyID = row.Field<string>("Currency ID")?.Trim(),
-                        CustomerID = row.Field<string>("Customer_ID")?.Trim(),
-                        CustomerName = row.Field<string>("Customer_Name")?.Trim()
-
+                        #region old
+                        //AmountApplied = row.Field<decimal>("Amount_Applied"),
+                        //AppliedToDocumentDate = row.Field<DateTime?>("Applied_to_Document_Date"),
+                        //ApplyDocumentDate = row.Field<DateTime?>("Apply_Document_Date"),
+                        //AppliedToDocTypeName = row.Field<string>("Applied_to_Doc_Type_Name")?.Trim(),
+                        //AppliedToDocNumber = row.Field<string>("Applied_to_Doc_Number")?.Trim(),
+                        //DocumentNumber = row.Field<string>("Document_Number")?.Trim(),
+                        //ExchangeRate = row.Field<decimal?>("Exchange Rate"),
+                        //DocumentDate = row.Field<DateTime?>("Document_Date"),
+                        //ShipmentNumber = row.Field<string>("Shipment number")?.Trim(),
+                        //Department = row.Field<string>("Department")?.Trim(),
+                        //Branch = row.Field<string>("Branch")?.Trim(),
+                        //ClientOrderNo = row.Field<string>("Client order No")?.Trim(),
+                        //BillOfLading = row.Field<string>("Bill of lading")?.Trim(),
+                        //CurrencyID = row.Field<string>("Currency ID")?.Trim(),
+                        //CustomerID = row.Field<string>("Customer_ID")?.Trim(),
+                        //CustomerName = row.Field<string>("Customer_Name")?.Trim()
+                        #endregion
+                        CustomerID = row["Customer ID"]?.ToString()?.Trim(),
+                        InvoiceNumber = row["Invoice Number"]?.ToString()?.Trim(),
+                        RemrningAmount = row["Remrning_Amount"] == DBNull.Value ? (decimal?)null : Convert.ToDecimal(row["Remrning_Amount"]),
+                        CURTRXAM = row["CURTRXAM"] == DBNull.Value ? (decimal?)null : Convert.ToDecimal(row["CURTRXAM"]),
+                        ORTRXAMT = row["ORTRXAMT"] == DBNull.Value ? (decimal?)null : Convert.ToDecimal(row["ORTRXAMT"]),
+                        CURNCYID = row["CURNCYID"]?.ToString()?.Trim(),
+                        XCHGRATE = row["XCHGRATE"] == DBNull.Value ? (decimal?)null : Convert.ToDecimal(row["XCHGRATE"]),
+                        CurrencyAmount = row["Currency amount"] == DBNull.Value ? (decimal?)null : Convert.ToDecimal(row["Currency amount"]),
+                        RMDTYPAL = row["RMDTYPAL"] == DBNull.Value ? (int?)null : Convert.ToInt32(row["RMDTYPAL"])
                     });
                 }
 
